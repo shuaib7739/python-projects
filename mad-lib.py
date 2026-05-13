9cb306b94d84e2094cc3498c19e94cceb24dda8c
shuaib abdi farah <shucebc1@gmail.com>
2026-05-13 18:44:02 +0300

    first project

---
+++ mad-lib.py
@@ -0,0 +1,19 @@
+noun1 = input("noun1 ")
+noun2 = input("noun2 ")
+verb1 = input("verb1 ")
+verb2 = input("verb2 ")
+adverb1 = input("adverb1 ")
+
+print(f"""Once upon a time, a little {noun1} named Goldilocks wandered into the forest and found a small cottage belonging to The Three Bears.
+Inside, she saw three bowls of porridge.
+She tasted the first bowl — it was too {adverb1}.
+The second bowl — too cold.
+The third bowl was just right, and she ate it all.
+Next, she tried three chairs.
+One was too big, one was too small  and the third was just right — but it broke when she sat on it.
+Then she went upstairs and found three {noun2}.
+One was too hard, one was too soft and the third was just right. Soon, she fell asleep.
+When the three bears came home, they noticed someone had been in their house. They found Goldilocks sleeping in the little bear’s bed.
+Startled awake, Goldilocks {verb1} up, ran out of the cottage, and hurried home through the forest.
+And she never {verb2} someone else's house again.""")
+
